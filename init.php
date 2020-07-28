a38b3dae57008b45cc3ab3107056628af5412def
Alikhan Izbergenov <alikhan@dnapayments.com>
2020-07-28 15:47:43 +0600

    CMS-57 init php utils

--- init.php
+++ init.php
@@ -6,3 +6,5 @@ require __DIR__ . '/lib/DNAPayments.php';
 
 
 require __DIR__ . '/lib/Util/Scope.php';
+require __DIR__ . '/lib/Util/HTTPRequester.php';
+require __DIR__ . '/lib/Util/LZCompressor\LZString.php';
