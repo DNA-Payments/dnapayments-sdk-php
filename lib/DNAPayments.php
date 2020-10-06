466ab424081ff06b7e5621df048a7ece21e9e04e
Alikhan Izbergenov <alikhan@dnapayments.com>
2020-10-06 20:19:07 +0600

    CMS-68 add refund

--- lib/DNAPayments.php
+++ lib/DNAPayments.php
@@ -5,6 +5,7 @@ namespace DNAPayments;
 use DNAPayments\Util\Scope;
 use DNAPayments\Util\HTTPRequester;
 use DNAPayments\Util\LZCompressor\LZString;
+use mysql_xdevapi\Exception;
 
 class DNAPayments {
     public function __construct($config = null) {
@@ -18,8 +19,10 @@ class DNAPayments {
     private static $fiels = [
         'authUrl' => 'https://oauth.dnapayments.com/oauth2/token',
         'testAuthUrl' => 'https://test-oauth.dnapayments.com/oauth2/token',
-        'testPaymentPageUrl' => 'https://test-pay.dnapayments.com/checkout',
-        'paymentPageUrl' => 'https://pay.dnapayments.com/checkout'
+        'testPaymentUrl' => 'https://test-pay.dnapayments.com',
+        'paymentUrl' => 'https://pay.dnapayments.com',
+        'testApiUrl' => 'https://test-api.dnapayments.com',
+        'apiUrl' => 'https://api.dnapayments.com'
     ];
 
 
@@ -43,35 +46,90 @@ class DNAPayments {
         if (self::$config['isTestMode']) {
             return (object) [
                 'authUrl' => self::$fiels['testAuthUrl'],
-                'paymentPageUrl' => self::$fiels['testPaymentPageUrl'],
+                'paymentUrl' => self::$fiels['testPaymentUrl'],
+                'apiUrl' => self::$fiels['testApiUrl'],
             ];
         }
         return (object) [
             'authUrl' => self::$fiels['authUrl'],
-            'paymentPageUrl' => self::$fiels['paymentPageUrl']
+            'paymentUrl' => self::$fiels['paymentUrl'],
+            'apiUrl' => self::$fiels['apiUrl']
         ];
     }
 
-    public static function auth($data)
-    {
-        $authData = [
-            'grant_type' => 'client_credentials',
-            'scope' => Scope::getScopes(self::$config['scopes']),
-            'client_id' => $data['client_id'],
-            'client_secret' => $data['client_secret'],
-            'terminal' => $data['terminal'],
-            'invoiceId' => strval($data['invoiceId']),
-            'amount' => floatval($data['amount']),
-            'currency' => strval($data['currency']),
-            'paymentFormURL' => array_key_exists('paymentFormURL', $data) ? $data['paymentFormURL'] : self::getBaseUrl() // todo: add
-        ];
+    private function refundAuth($data) {
+        try {
+            $authData = [
+                'grant_type' => 'client_credentials',
+                'scope' => 'webapi',
+                'client_id' => $data['client_id'],
+                'client_secret' => $data['client_secret'],
+                'terminal' => $data['terminal'],
+                'invoiceId' => strval($data['invoiceId']),
+                'amount' => floatval($data['amount']),
+                'currency' => strval($data['currency'])
+            ];
 
-        $response = HTTPRequester::HTTPPost(self::getPath()->authUrl, [], $authData);
-        if ($response != null && $response['status'] >= 200 && $response['status'] < 400) {
-            return $response['response'];
+            $response = HTTPRequester::HTTPPost(self::getPath()->authUrl, [], $authData);
+            if ($response != null && $response['status'] >= 200 && $response['status'] < 400) {
+                return $response['response'];
+            }
+
+            throw new \Exception('Error: No auth service');
+        } catch (Exception $e) {
+            throw $e;
         }
+    }
 
-        throw new \Error('Error: No auth service');
+    private function refundRequest($token, $transaction_id, $amount) {
+        try {
+            $response = HTTPRequester::HTTPPost(self::getPath()->apiUrl . '/transaction/operation/refund', array(
+                'Authorization' => 'Bearer ' . $token
+            ), array(
+                'id' => $transaction_id,
+                'amount' => $amount
+            ) );
+
+            if ($response != null && $response['status'] >= 200 && $response['status'] < 400) {
+                return $response['response'];
+            }
+
+            throw new \Exception('Error: Refund request error');
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+
+    public function refund($data) {
+        $auth = self::refundAuth($data);
+        $response = self::refundRequest($auth['access_token'], $data['transaction_id'], $data['amount']);
+        return $response;
+    }
+
+    public static function auth($data) {
+        try {
+            $authData = [
+                'grant_type' => 'client_credentials',
+                'scope' => Scope::getScopes(self::$config['scopes']),
+                'client_id' => $data['client_id'],
+                'client_secret' => $data['client_secret'],
+                'terminal' => $data['terminal'],
+                'invoiceId' => strval($data['invoiceId']),
+                'amount' => floatval($data['amount']),
+                'currency' => strval($data['currency']),
+                'paymentFormURL' => array_key_exists('paymentFormURL', $data) ? $data['paymentFormURL'] : self::getBaseUrl() // todo: add
+            ];
+
+            $response = HTTPRequester::HTTPPost(self::getPath()->authUrl, [], $authData);
+            if ($response != null && $response['status'] >= 200 && $response['status'] < 400) {
+                return $response['response'];
+            }
+            throw new \Exception('Error: No auth service');
+
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 
     private static function getBaseUrl() {
@@ -85,7 +143,7 @@ class DNAPayments {
 
     public static function generateUrl($order, $authToken)
     {
-        return self::getPath()->paymentPageUrl . '/?params=' . self::encodeToUrl((object) [
+        return self::getPath()->paymentUrl . '/checkout/?params=' . self::encodeToUrl((object) [
                 'auth' => $authToken,
                 'invoiceId' => strval($order['invoiceId']),
                 'terminal' => $order['terminal'],
