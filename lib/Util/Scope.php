b399060498aa79301775f0e341a44749452fb853
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2022-01-28 15:35:35 +0600

    fix error with scopes

--- lib/Util/Scope.php
+++ lib/Util/Scope.php
@@ -8,13 +8,13 @@ class Scope {
     private static $INTEGRATION_SEAMLESS = 'integration_seamless';
     public static function getScopes($config) {
         $scopes = ['payment'];
-        if (array_key_exists('allowHosted', $config)) {
+        if (array_key_exists('allowHosted', $config) && $config['allowHosted']) {
             array_push($scopes, self::$INTEGRATION_HOSTED);
         }
-        if (array_key_exists('allowEmbedded', $config)){
+        if (array_key_exists('allowEmbedded', $config) && $config['allowEmbedded']){
             array_push($scopes, self::$INTEGRATION_EMBEDDED);
         }
-        if (array_key_exists('allowSeamless', $config)) {
+        if (array_key_exists('allowSeamless', $config) && $config['allowSeamless']) {
             array_push($scopes, self::$INTEGRATION_SEAMLESS);
         }
         return implode(' ', $scopes);
